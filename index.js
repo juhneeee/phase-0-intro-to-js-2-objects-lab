a303c5c998e594ad7246686ddfa674714348b33d
Johnny Chen <johnnyc1314@gmail.com>
2022-04-05 13:52:59 -0400

    commit tree 53044264664f4542177631580e7f783f04f18dbb

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Joe',
+    streetAdress: 'Big Apple',
+}
+function updateEmployeeWithKeyAndValue(empObj,key, value){
+    let helper = {...empObj};
+    helper[key] = value;
+    return helper;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(empObj, key, value){
+    empObj[key] = value;
+    return empObj;
+}
+function destructivelyDeleteFromEmployeeByKey(empObj, key){
+    delete empObj[key];
+    return empObj
+}
+function deleteFromEmployeeByKey(empObj, key){
+    let helper = {...empObj};
+    delete helper[key];
+    return helper
+}
